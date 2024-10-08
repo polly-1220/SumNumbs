c57a6de58a72cd73a8f7acda90e0f5f8cdb211e6
polly-1220 <bondarenko.p@academy.com.ua>
2024-10-08 15:44:10 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int number1 =  5;
+    int number2 =  10;
+    int sum = number1 + number2;
+
+  System.out.println("The sum is: " + sum);
+  }
+}
