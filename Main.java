c0b66eafa27c75cd422bf72a7c3ad49c49588263
polly-1220 <bondarenko.p@academy.com.ua>
2024-10-08 15:45:57 +0300

    Update values

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    int number1 =  5;
-    int number2 =  10;
+    int number1 =  25;
+    int number2 =  75;
     int sum = number1 + number2;
 
   System.out.println("The sum is: " + sum);
